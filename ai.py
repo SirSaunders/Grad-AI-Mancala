644621000c9ce4df46e7f3edea3fe152ecf2fda4
Johnathan Saunders <Saundersokj@gmail.com>
2018-04-09 14:39:26 -0400

    fixed all the syntx errors caused by nano

--- ai.py
+++ ai.py
@@ -1,43 +1,148 @@
+board = {
+    "board": {
+        "space": [{
+            "type": "mancala",
+            "marbles": 0,
+            "space_id": 0,
+            "player": 1
+        },
+            {
+                "type": "normal",
+                "marbles": 4,
+                "space_id": 1,
+                "player": 0
+            },
+            {
+                "type": "normal",
+                "marbles": 4,
+                "space_id": 2,
+                "player": 0
+            },
+            {
+                "type": "normal",
+                "marbles": 4,
+                "space_id": 3,
+                "player": 0
+            },
+            {
+                "type": "normal",
+                "marbles": 4,
+                "space_id": 4,
+                "player": 0
+            },
+            {
+                "type": "normal",
+                "marbles": 4,
+                "space_id": 5,
+                "player": 0
+            },
+            {
+                "type": "normal",
+                "marbles": 4,
+                "space_id": 6,
+                "player": 0
+            },
+            {
+                "type": "mancala",
+                "marbles": 0,
+                "space_id": 7,
+                "player": 0
+            },
+            {
+                "type": "normal",
+                "marbles": 4,
+                "space_id": 8,
+                "player": 1
+            },
+            {
+                "type": "normal",
+                "marbles": 4,
+                "space_id": 9,
+                "player": 1
+            },
+            {
+                "type": "normal",
+                "marbles": 4,
+                "space_id": 10,
+                "player": 1
+            },
+            {
+                "type": "normal",
+                "marbles": 4,
+                "space_id": 11,
+                "player": 1
+            },
+            {
+                "type": "normal",
+                "marbles": 4,
+                "space_id": 12,
+                "player": 1
+            },
+            {
+                "type": "normal",
+                "marbles": 4,
+                "space_id": 13,
+                "player": 1
+            }
+        ]
+    }
+}
+
 boardLength = 14
 
-def goAgainPoints(move, board):
-	landedSpace = board[move[0]]
-	if landedSpace['type'] == 'mancala':
-		return 30
-	return 0
 
-def emptySpacePoints(move, board):
-        landedSpace = board[move[0]]
-        if landedSpace['marbles'] == 0:
-               	accrossSpace= board[(move[0] + (boardLength / 2)) % boardLength]
-		return accrossSpace['marbles'] * 10
-	return 0
+def go_again_points(move, board):
+    landedSpace = board[move[0]]
+    if landedSpace['type'] == 'mancala':
+        return 30
+    return 0
 
-def incrementMancalaPoints(move, board):
-	return 10*move[1]
+
+def empty_space_points(move, board):
+    landedSpace = board[move[0]]
+    if landedSpace['type'] != 'mancala' and landedSpace['marbles'] == 0:
+        accrossSpace = board[int((move[0] + (boardLength / 2)) % boardLength)]
+        return accrossSpace['marbles'] * 10
+    else:
+        return 0
+
+
+def increment_mancala_points(move):
+    return 10 * move[1]
+
+
+def getMove(pos, marbles, player, board):
+    incrementedMancala = 0
+    currentPos = pos
+    yourSideScore = 0
+    for i in range(marbles):
+        currentPos = (currentPos + 1) % boardLength
+        space = board[currentPos]
+        if space['player'] == player:
+            yourSideScore += 1
+        if (space['type'] == 'mancala'):
+            if space['player'] == player:
+                incrementedMancala += 1
+            else:
+                currentPos = (currentPos + 1) % boardLength
+    return currentPos, incrementedMancala, yourSideScore
+
 
 def findPoints(moveFromPos, board):
-        moveSpace = board[moveFromPos]
-        marbles = moveSpace['marbles']
-        move = getMove(moveSpace['space_id'],marbles,moveSpace['player'],board)
-	incrementMancalaPoints = incrementMancalaPoints(move)
-	goAgainPoints = goAgainPoints(move,board)
-	emptySpacePoints = emptySpacePoints(move,board)
-	yourSideScore = move[2]
+    moveSpace = board[moveFromPos]
+    marbles = moveSpace['marbles']
+    move = getMove(moveSpace['space_id'], marbles, moveSpace['player'], board)
+    incrementMancalaPoints = increment_mancala_points(move)
+    goAgainPoints = go_again_points(move, board)
+    emptySpacePoints = empty_space_points(move, board)
+    yourSideScore = move[2]
+    print(incrementMancalaPoints)
+    print(goAgainPoints)
+    print(emptySpacePoints)
+    print(yourSideScore)
+    print(move)
+    return incrementMancalaPoints + goAgainPoints + emptySpacePoints + yourSideScore
 
 
-def getMove(pos, marbles, player, board)
-	incrementedMancala = 0
-	currentPos = pos
-	yourSideScore = 0
-	for(i in range(0, marbles):
-		currentPos = (currentPos + 1) % boardLength
-		space = board[currentPos]
-		if space['player'] == player:
-			yourSideScore += 1
-		if(space['type'] = 'mancala'):
-			if space['player'] == player:
-				incrementedMancala += 1
-			else:
-				currentPos = (currentPos + 1) % boardLength
-	return (currentPos, incrementedMancala, yourSideScore)
+if __name__ == '__main__':
+    print(findPoints(3,board['board']['space']))
